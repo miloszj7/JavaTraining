80ff030f6f7955c9b0ad9e24b254cf553b57773c
Miloszek <miloszj7@gmail.com>
2012-10-27 17:51:55 +0200

    better text

--- src/com/test/JavaTest.java
+++ src/com/test/JavaTest.java
@@ -10,7 +10,7 @@ public class JavaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi there! How are you?");
-		System.out.println( new Date());
+		System.out.println("Today is: " + new Date());
 	}
 
 }
