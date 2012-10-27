9479a46e10ad728f49a08719e2cdb7bd21acbf3f
Miloszek <miloszj7@gmail.com>
2012-10-27 16:55:48 +0200

    New printed text

--- src/com/test/JavaTest.java
+++ src/com/test/JavaTest.java
@@ -7,7 +7,7 @@ public class JavaTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi there!");
+		System.out.println("Hi there! How are you?");
 	}
 
 }
